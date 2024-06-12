af0ca4d73046285741da4a21d08ad6c8811c6267
myidhfh <2259278124@qq.com>
2024-06-12 13:04:22 +0800

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('guonei-folder').addEventListener('click', function() {
+        window.location.href = 'guonei.html';
+    });
+
+    document.getElementById('guowai-folder').addEventListener('click', function() {
+        window.location.href = 'guowai.html';
+    });
+});
